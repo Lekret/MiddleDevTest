72cd187a7503b9db0f0661bb43ea282d305d3f65
Lekret <lenicraft@mail.ru>
2022-09-30 14:34:23 +0300

    PathFinder input validation

--- Assets/Rectangles/Scripts/PathFinder.cs
+++ Assets/Rectangles/Scripts/PathFinder.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Rectangles.Scripts
@@ -7,12 +9,20 @@ namespace Rectangles.Scripts
     {
         public IEnumerable<Vector2> GetPath(Vector2 source, Vector2 target, IEnumerable<Edge> edges)
         {
-            var result = new List<Vector2> {source};
+            if (!edges.FirstOrDefault().First.Contains(source))
+                return Array.Empty<Vector2>();
+
+            if (!edges.LastOrDefault().Second.Contains(target))
+                return Array.Empty<Vector2>();
+            
+            var result = new List<Vector2>();
+            result.Add(source);
             foreach (var edge in edges)
             {
                 var edgeMid = (edge.Start + edge.End) / 2f;
                 result.Add(edgeMid);
             }
+            
             result.Add(target);
             return result;
         }
