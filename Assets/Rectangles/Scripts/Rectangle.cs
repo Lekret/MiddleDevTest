c6b1007c43d4eee941a130aa10b1218838d09010
Lekret <lenicraft@mail.ru>
2022-09-30 14:53:03 +0300

    validation

--- Assets/Rectangles/Scripts/Rectangle.cs
+++ Assets/Rectangles/Scripts/Rectangle.cs
@@ -12,8 +12,16 @@ namespace Rectangles.Scripts
 
     public static class RectangleExtensions
     {
+        public static bool IsValid(this Rectangle rectangle)
+        {
+            return rectangle.Max.x > rectangle.Min.x &&
+                   rectangle.Max.y > rectangle.Min.y;
+        }
+        
         public static bool Contains(this Rectangle rectangle, Vector2 point)
         {
+            if (!rectangle.IsValid())
+                return false;
             return point.x >= rectangle.Min.x &&
                    point.y >= rectangle.Min.y &&
                    point.x <= rectangle.Max.x &&
