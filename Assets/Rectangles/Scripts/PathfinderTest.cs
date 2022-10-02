1a6a704e4564504d3b11940c7e227c6e2eb1d0de
Lekret <lekret399@mail.ru>
2022-10-02 09:38:32 +0300

    Draw positions

--- Assets/Rectangles/Scripts/PathfinderTest.cs
+++ Assets/Rectangles/Scripts/PathfinderTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,6 +11,8 @@ namespace Rectangles.Scripts
         [SerializeField] private Vector2 _from;
         [SerializeField] private Vector2 _to;
         [SerializeField] private Edge[] _edges;
+
+        private Vector3[] _positions = Array.Empty<Vector3>();
 
         [ContextMenu("CalcPath")]
         private void CalcPath()
@@ -36,14 +39,39 @@ namespace Rectangles.Scripts
 
             CalcPath();
         }
-
+        
         private void OnDrawGizmos()
+        {
+            DrawSourceWithTarget();
+            DrawLineRendererPoints();
+            DrawRectangles();
+        }
+
+        private void DrawSourceWithTarget()
         {
             Gizmos.color = Color.magenta;
             Gizmos.DrawWireSphere(_from, 0.5f);
             Gizmos.DrawWireSphere(_to, 0.5f);
             Gizmos.color = Color.white;
-            
+        }
+        
+        private void DrawLineRendererPoints()
+        {
+            Gizmos.color = Color.yellow;
+            if (_positions == null || _positions.Length < _lineRenderer.positionCount)
+                _positions = new Vector3[_lineRenderer.positionCount];
+
+            _lineRenderer.GetPositions(_positions);
+            foreach (var p in _positions)
+            {
+                Gizmos.DrawWireSphere(p, 0.3f);
+            }
+
+            Gizmos.color = Color.white;
+        }
+
+        private void DrawRectangles()
+        {
             foreach (var edge in _edges)
             {
                 DrawRectangle(edge.First);
@@ -53,7 +81,7 @@ namespace Rectangles.Scripts
                 Gizmos.color = Color.white;
             }
         }
-
+        
         private static void DrawRectangle(Rectangle rect)
         {
             var center = (rect.Min + rect.Max) / 2f;
